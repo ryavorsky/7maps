07f386b99591129cc7f85b378cf44038fa12e913
igorshkov <bsfireworks@yandex.ru>
2016-06-02 15:14:28 +0300

    start.sh updated

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,11 @@ count_members=$(python3 count_group_members.py 2>&1)
 
 count_members_per_acc=$((($count_members+$count_accs-1)/$count_accs))
 
-for ((i=0;i<($count_accs);i++)); do
-    nohup python3 members_info.py $(($i*$count_members_per_acc)) $((count_members_per_acc)) $i > "$i.out" &
+for i in $(seq 0 $((count_accs-1)))
+do
+    nohup python3 members_info.py $(($i*$count_members_per_acc)) $((count_membe$
 done
+
+
+
+
